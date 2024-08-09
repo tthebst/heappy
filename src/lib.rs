f2bf46026cae7a94fdb0cbc9f50564d95b2cb29f
Tim Gretler <tim.gretler@dfinity.org>
2024-08-09 12:49:56 +0000

    .

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,6 @@ mod collector;
 #[cfg(feature = "enable_heap_profiler")]
 mod hook;
 
-#[cfg(feature = "jemalloc_shim")]
-mod jemalloc_adapter;
-
-#[cfg(feature = "jemalloc_shim")]
-use jemalloc_adapter as adapter;
-
 // On linux you need to reference at least one symbol in a module if we want it be be actually linked.
 // Otherwise the hooks like `pub unsafe extern "C" fn malloc(size: size_t) -> *mut c_void` defined in the shim
 // module won't override the respective weak symbols from libc, since they don't ever get linked in the final executable.
